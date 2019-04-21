8de64abf75ff07219f577fb13303f32d1855b9b6
drd <idone.leonardo@gmail.com>
2019-04-21 22:09:29 +0200

    Riordinata la posizione di alcuni file nel progetto (Constants/MainPage) e il loro namespace/assembly

--- rentx_app/RentIT/RentIT/RentIT/Data/Constants.cs
+++ rentx_app/RentIT/RentIT/RentIT/Data/Constants.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace RentIT
+namespace RentIT.Views
 {
     public static class Constants
     {
--- rentx_app/RentIT/RentIT/RentIT/Views/MainPage.xaml.cs
+++ rentx_app/RentIT/RentIT/RentIT/Views/MainPage.xaml.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace RentIT
+namespace RentIT.Views
 {
     public partial class MainPage : ContentPage
     {
