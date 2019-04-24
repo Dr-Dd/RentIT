365491d0d1fa5564e5ab0d4c2fa31b905bb368c7
CarloIurato13 <carloiurato@virgilio.it>
2019-04-24 17:12:30 +0200

    no message

---
+++ rentx_app/RentIT/RentIT/RentIT/Models/MenuItems.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentIT.Models
+{
+    public class MenuItems
+    {
+        public String Title { get; set; }
+
+        public String Icon { get; set; }
+
+        public String TypeTarget { get; set; }
+    }
+}
