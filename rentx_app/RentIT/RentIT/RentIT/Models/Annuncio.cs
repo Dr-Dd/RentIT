28a8965bcc360b0d890ab173d33e099eb9306216
CarloIurato13 <carloiurato@virgilio.it>
2019-05-09 18:05:45 +0200

    Creata una bozza di TilePage.xaml e la classe Annuncio.cs sistemata in Models e ancora da sistemare.
    I campi di Annuncio mi servono semplicemente per verificare la correttezza della mia pagina

---
+++ rentx_app/RentIT/RentIT/RentIT/Models/Annuncio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace RentIt.Models
+{
+    class Annuncio
+    {
+        public string NomeOggetto { get; set; }
+
+        public string Descrizione { get; set; }
+
+        public decimal Prezzo { get; set; }
+
+        public string NomeAffittuario { get; set; }
+
+        public string CognomeAffittuario { get; set; }
+
+        public string Posizione { get; set; }
+
+        public DateTime Data { get; set; }
+
+    }
+}
