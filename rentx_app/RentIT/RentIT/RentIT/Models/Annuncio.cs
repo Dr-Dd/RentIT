49a94610924ec619377c9fb69e9b020d8c6c0402
CarloIurato13 <carloiurato@virgilio.it>
2019-07-04 17:43:17 +0200

    Aggiunto l'utente richiedente nel Model annuncio

--- rentx_app/RentIT/RentIT/RentIT/Models/Annuncio.cs
+++ rentx_app/RentIT/RentIT/RentIT/Models/Annuncio.cs
@@ -18,6 +18,8 @@ namespace RentIT.Models
 
         public string NomeAffittuario { get; set; }
 
+        public string NomeRichiedente { get; set; }
+
         public string Posizione { get; set; }
 
         public DateTime Data { get; set; }
