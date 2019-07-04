6be67f120f432decc892f7ccd643da620882b72f
wild0rez <mau.dirollo@stud.uniroma3.it>
2019-07-04 12:24:33 +0200

    Creato backend in Spring

---
+++ backend/rentx_backend/rentx_backend/src/main/java/it/rentx/backend/RentxBackendApplication.java
@@ -0,0 +1,13 @@
+package it.rentx.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentxBackendApplication.class, args);
+	}
+
+}
