ffb5e1576b8fe1deeacd89f34d7b124b60988b04
wild0rez <mau.dirollo@stud.uniroma3.it>
2019-07-04 11:53:51 +0200

    Eliminato backend in python

--- backend/test.py
+++
@@ -1,7 +0,0 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-
-password = "canevolante"
-salt = "1234567890Abc"
-pass_hash = generate_password_hash(password)
-print(pass_hash)
-print(check_password_hash(password,pass_hash))
