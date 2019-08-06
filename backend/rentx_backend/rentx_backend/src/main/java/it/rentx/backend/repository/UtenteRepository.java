5c4bca857e45739dc9e020eab82ef1b87ad14ed2
wild0rez <mau.dirollo@stud.uniroma3.it>
2019-08-06 20:22:03 +0200

    Aggiunto metodo eliminazione account
    E ulteriori modifiche (devo sistemare il metodo per modificare l'utente)

--- backend/rentx_backend/rentx_backend/src/main/java/it/rentx/backend/repository/UtenteRepository.java
+++ backend/rentx_backend/rentx_backend/src/main/java/it/rentx/backend/repository/UtenteRepository.java
@@ -1,5 +1,7 @@
 package it.rentx.backend.repository;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,6 @@ import it.rentx.backend.models.Utente;
 @Repository
 public interface UtenteRepository extends JpaRepository<Utente, Long>{
 	
+	@Transactional
 	public Utente findByEmail(String email);
 }
