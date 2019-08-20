c729b631e5070308a77eba15cc751b6b4282457b
berenice02 <foderaro.elisa@gmail.com>
2019-08-20 19:05:09 +0200

    Aggiunto il command modificaDati. Da finire perché bisogna passargli il nuovo utente

--- rentx_app/RentIT/RentIT/RentIT/ViewModels/ModificaDatiViewModel.cs
+++ rentx_app/RentIT/RentIT/RentIT/ViewModels/ModificaDatiViewModel.cs
@@ -10,6 +10,7 @@ using System.IO;
 using RentIT.Services.User;
 using App.Services.Foto;
 using RentIT.Models.User;
+using App.Models.User;
 
 namespace RentIT.ViewModels
 {
@@ -48,7 +49,71 @@ namespace RentIT.ViewModels
 
         public override async Task Init()
         {
+            Utente = await _userService.GetCurrentProfileAsync();
+            //TODO: da decommentare dopo fatto il collegamento al db
+            //Immagine = await _fotoService.getPropic();
+        }
 
+        //Metodo per prendere l'immagine profilo dal database
+        public async Task<Image> getPropic()
+        {
+            ImageModel foto = await _userService.GetUserImage();
+            Image img = null;
+            if (foto != null)
+                img = _fotoService.fromStringToImage(foto.data);
+            return img;
+
+        }
+
+        //Comando per aggiungere o modificare la propic
+        Command _modificaFotoCommand;
+        public Command ModificaFotoCommand
+        {
+            get
+            {
+                return _modificaFotoCommand
+                    ?? (_modificaFotoCommand = new Command(async () => await ExecuteModificaFotoCommand()));
+            }
+        }
+
+        async Task ExecuteModificaFotoCommand()
+        {
+            //Caricare un'immagine dalla galleria
+            Stream stream = await DependencyService.Get<IPicturePicker>().GetImageStreamAsync();
+
+            if (stream != null)
+            {
+                //se esiste, si salva la foto nel db associandola all'utente
+                string base64 = _fotoService.fromStreamToString(stream);
+                await _userService.UploadUserImageAsync(base64);
+                OnPropertyChanged();
+            }
+        }
+
+        /*Comando per modificare i dati dell'account*/
+        Command _modificaDatiCommand;
+        public Command ModificaDatiCommand
+        {
+            get
+            {
+                return _modificaDatiCommand
+                    ?? (_modificaDatiCommand = new Command(async () => await ExecuteModificaDatiCommand()));
+            }
+        }
+
+        async Task ExecuteModificaDatiCommand()
+        {
+            IsBusy = true;
+            var response = await _userService.ModifyUserData(/*TODO: utente*/);
+            if (response.hasSucceded)
+            {
+                await NavService.NavigateToMainPage();
+            }
+            else
+            {
+                await App.Current.MainPage.DisplayAlert("Errore", response.responseMessage, "Ok");
+            }
+            IsBusy = false;
         }
 
         /*Comando per eliminare l'account*/
@@ -75,30 +140,6 @@ namespace RentIT.ViewModels
                 await App.Current.MainPage.DisplayAlert("Errore", response.responseMessage, "Ok");
             }
             IsBusy = false;
-        }
-
-        //Comando per aggiungere o modificare la propic
-        Command _modificaFotoCommand;
-        public Command ModificaFotoCommand
-        {
-            get
-            {
-                return _modificaFotoCommand
-                    ?? (_modificaFotoCommand = new Command(async () => await ExecuteModificaFotoCommand()));
-            }
-        }
-
-        async Task ExecuteModificaFotoCommand()
-        {
-            //Caricare un'immagine dalla galleria
-            Stream stream = await DependencyService.Get<IPicturePicker>().GetImageStreamAsync();
-
-            if (stream != null)
-            {
-                //se esiste, si salva nel db associato all'utente
-                string base64 = _fotoService.fromStreamToString(stream);
-                await _userService.UploadUserImageAsync(base64);
-            }
         }
     }
 }
