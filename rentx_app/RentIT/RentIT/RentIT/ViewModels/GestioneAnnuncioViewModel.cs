cd3f4d6c5cdb231b77d1c939f810872b2f1889f1
berenice02 <foderaro.elisa@gmail.com>
2019-09-11 16:15:48 +0200

    Abbozzati v e vm di gestioneAnnuncio

--- rentx_app/RentIT/RentIT/RentIT/ViewModels/GestioneAnnuncioViewModel.cs
+++ rentx_app/RentIT/RentIT/RentIT/ViewModels/GestioneAnnuncioViewModel.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace RentIT.ViewModels
 {
@@ -30,6 +31,78 @@ namespace RentIT.ViewModels
         public async override Task Init(Annuncio annuncio)
         {
             Annuncio = annuncio;
+        }
+
+        /*Comando per eliminare l'annuncio*/
+        Command _eliminaAnnuncioCommand;
+        public Command EliminaAnnuncioCommand
+        {
+            get
+            {
+                return _eliminaAnnuncioCommand
+                    ?? (_eliminaAnnuncioCommand = new Command(async () => await ExecuteEliminaAnnuncioCommand()));
+            }
+        }
+
+        async Task ExecuteEliminaAnnuncioCommand()
+        {
+            IsBusy = true;
+            /*
+            var response = await _annuncioService.Delete();
+            if (response.hasSucceded)
+            {
+                await NavService.NavigateToMainPage();
+            }
+            else
+            {
+                await App.Current.MainPage.DisplayAlert("Errore", response.responseMessage, "Ok");
+            }
+            */
+
+            IsBusy = false;
+        }
+
+        bool EmptyFields()
+        {
+            var empty = string.IsNullOrWhiteSpace(Annuncio.NomeOggetto) ||
+                        string.IsNullOrWhiteSpace(Annuncio.Descrizione) ||
+                        (Annuncio.Prezzo == 0);
+            return empty;
+        }
+
+        /*Comando per modificare l'annuncio*/
+        Command _modificaAnnuncioCommand;
+        public Command ModificaAnnuncioCommand
+        {
+            get
+            {
+                return _modificaAnnuncioCommand
+                    ?? (_modificaAnnuncioCommand = new Command(async () => await ExecuteModificaAnnuncioCommand()));
+            }
+        }
+
+        async Task ExecuteModificaAnnuncioCommand()
+        {
+            IsBusy = true;
+
+            if (!EmptyFields())
+            {
+                await App.Current.MainPage.DisplayAlert("Errore", "Non puoi lasciare campi vuoti", "Ok");
+                return;
+            }
+
+            /*
+            var response = await _annuncioService.ModifyData(Annuncio);
+            if (response.hasSucceded)
+            {
+                await NavService.NavigateToMainPage();
+            }
+            else
+            {
+                await App.Current.MainPage.DisplayAlert("Errore", response.responseMessage, "Ok");
+            }
+            */
+            IsBusy = false;
         }
     }
 }
